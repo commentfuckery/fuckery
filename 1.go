d19be10a2d0f2bfc8dd800e104eb1cf21a9f0840
ossian <the5heepdev@gmail.com>
2017-09-04 17:34:51 -0400

    Create 1.go

---
+++ 1.go
@@ -0,0 +1,5 @@
+// wtf has function to create from byte and not read from??
+// connect that bitch
+// for some reason the lib is a bit retarded and spams the channel
+// TODO: bitch at lib bc only gives me access to a reader/writer.
+// TODO: add more evil ass fucking shit
